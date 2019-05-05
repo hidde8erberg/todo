b4346fc810a6a9173c85c586fbf6d5fe5d3b10e6
hidde8erberg <hidde8erberg@gmail.com>
2019-05-05 17:50:19 +0200

    package-lock.json added

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mustache = require('mustache-express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 
-//mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/todo', {useNewUrlParser: true}).then( () => {
-    console.log('Database connected.');
-});
+    console.log('Database connected');
+}).catch(err => console.log(err));
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 const m = mustache()
 m.cache = null;
@@ -19,6 +18,6 @@ app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 app.use('/', routes);
 
-app.listen(3000, (req, res) => {
+app.listen(3000, () => {
     console.log('listening on port 3000...');
 });
